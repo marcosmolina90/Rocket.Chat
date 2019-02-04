59ad7842d9e8f40c22b3a614e71970ad9a8cebcf
marcos.molina90@gmail.com <marcos.molina@intranet.maxiconsystems.com.br>
2019-02-04 16:22:33 -0200

    merge

--- packages/rocketchat-webrtc/package.js
+++ packages/rocketchat-webrtc/package.js
@@ -6,20 +6,6 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-<<<<<<< HEAD
-	api.use('rocketchat:lib');
-	api.use('ecmascript');
-
-	api.use('templating', 'client');
-	api.mainModule('client/WebRTCClass.js', 'client');
-	api.addFiles('client/adapter.js', 'client');
-	// api.addFiles(');
-	api.addFiles('client/screenShare.js', 'client');
-
-	api.addFiles('server/settings.js', 'server');
-
-	api.export('WebRTC', 'client');
-=======
 	api.use([
 		'ecmascript',
 		'rocketchat:utils',
@@ -31,5 +17,4 @@ Package.onUse(function(api) {
 	]);
 	api.mainModule('client/index.js', 'client');
 	api.mainModule('server/index.js', 'server');
->>>>>>> 9d7d2705b884d01ccff402c26cd9e38006181825
 });
